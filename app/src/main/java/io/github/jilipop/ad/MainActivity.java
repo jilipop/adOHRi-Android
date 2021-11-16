98a80cc2349536fe6a32900604606b2b5e023f48
jilipop <jilipop@web.de>
2021-11-16 18:00:53 +0100

    first attempt at writing directly to audio stream

--- app/src/main/java/io/github/jilipop/ad/MainActivity.java
+++ app/src/main/java/io/github/jilipop/ad/MainActivity.java
@@ -2,6 +2,7 @@ package io.github.jilipop.ad;
 
 import android.content.Context;
 import android.net.wifi.WifiManager;
+import android.os.PowerManager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import io.github.jilipop.ad.databinding.ActivityMainBinding;
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    private WifiManager.WifiLock wifiLock;
+    private PowerManager.WakeLock wakeLock;
+
     private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
     @Override
@@ -26,8 +30,31 @@ public class MainActivity extends AppCompatActivity {
 
         binding.sampleText.setText("something bla");
 
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiLock = wifiManager.createWifiLock(WifiManager.WIFI_MODE_FULL_HIGH_PERF, "AD:WifiLock");
+        wifiLock.acquire();
+        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "AD:WakeLock");
+        wakeLock.acquire(3*60*60*1000L /*3 hours*/);
+
         AdReceiver.create(this);
         AdReceiver.start();
-        //executorService.schedule((Runnable) AdReceiver::stop, 10, TimeUnit.SECONDS);
+
+        //executorService.schedule((Runnable) AdReceiver::stop, 20, TimeUnit.SECONDS);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (wakeLock != null) {
+            if (wakeLock.isHeld()) {
+                wakeLock.release();
+            }
+        }
+        if (wifiLock != null) {
+            if (wifiLock.isHeld()) {
+                wifiLock.release();
+            }
+        }
     }
 }
