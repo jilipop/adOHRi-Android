a1537d09aea1283c8f55fa1701407ff52177d533
jilipop <jilipop@web.de>
2022-04-21 22:57:52 +0200

    rename hostIP to subnet and throw out the last octet - gonna be using this to check if the dhcp range matches rather than for pinging the access point

--- app/src/main/java/io/github/jilipop/adohri/Secrets.java
+++ app/src/main/java/io/github/jilipop/adohri/Secrets.java
@@ -3,5 +3,5 @@ package io.github.jilipop.adohri;
 public class Secrets {
     public static final String ssid = "Audio-Deskription";
     public static final String password = "klappeauf!";
-    public static final String hostIP = "10.3.141.1";
+    public static final String subnet = "10.3.141";
 }
