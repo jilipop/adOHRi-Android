3664bf14a00c8e66b92b9fb45e8f9db2531f9fc8
jilipop <jilipop@web.de>
2022-04-25 23:02:05 +0200

    add action bar navigation and new info and licenses activities

---
+++ app/src/main/java/io/github/jilipop/adohri/LicensesActivity.java
@@ -0,0 +1,40 @@
+package io.github.jilipop.adohri;
+
+import android.view.Menu;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import io.github.jilipop.adohri.databinding.ActivityLicensesBinding;
+import io.github.jilipop.adohri.databinding.ActivityMainBinding;
+
+public class LicensesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_licenses);
+
+        Toolbar toolbar = findViewById(R.id.licenses_toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(R.string.title_activity_licenses);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        WebView licensesView = findViewById(R.id.licenses_view);
+        licensesView.getSettings().setJavaScriptEnabled(false);
+        licensesView.loadUrl("file:///android_asset/licenses.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.licenses_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
+}
