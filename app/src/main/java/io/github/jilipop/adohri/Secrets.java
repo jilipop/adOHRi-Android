ef1d52cd02d78bb684fabbcf4f3eb6445fd4c6c1
Hubert Popiolek <59879082+jilipop@users.noreply.github.com>
2022-04-25 23:10:30 +0200

    Delete Secrets.java
    shouldn't be part of git

--- app/src/main/java/io/github/jilipop/adohri/Secrets.java
+++
@@ -1,7 +0,0 @@
-package io.github.jilipop.adohri;
-
-public class Secrets {
-    public static final String ssid = "Audio-Deskription";
-    public static final String password = "klappeauf!";
-    public static final String subnet = "10.3.141";
-}
